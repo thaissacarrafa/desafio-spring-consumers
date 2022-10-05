2136fd002bd8d5512b565da96aab30dac5a48edb
thaissacarrafa <thaissacarrafa@gmail.com>
2022-10-05 16:10:46 -0300

    first commit

---
+++ src/main/java/com/meli/desafiospringconsumers/DesafioSpringConsumersApplication.java
@@ -0,0 +1,13 @@
+package com.meli.desafiospringconsumers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringConsumersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringConsumersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/desafiospringconsumers/DesafioSpringConsumersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.desafiospringconsumers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringConsumersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
